930b71a75458b01eb324557300fb36f1e6925ef4
JaimeAdele <jaime.zuspann@gmail.com>
2022-03-25 13:06:44 -0700

    Simplify CrimeLab structure for efficient crime lookup

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -11,7 +12,7 @@ import java.util.UUID;
 // CrimeLab is a singleton class
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
-    private List<Crime> mCrimes;
+//    private List<Crime> mCrimes;
     private Map<UUID, Crime> mCrimeMap; // For more efficient crime lookup
 
     public static CrimeLab get(Context context) {
@@ -22,19 +23,19 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
-        mCrimes = new ArrayList<>();
-        mCrimeMap = new HashMap<>();
+//        mCrimes = new ArrayList<>();
+        mCrimeMap = new LinkedHashMap<>();
         for (int i = 0; i < 100; i++) {
             Crime crime = new Crime();
             crime.setTitle("Crime #" + i);
             crime.setSolved(i % 2 == 0); // Every other crime will be set to Solved
-            mCrimes.add(crime);
+//            mCrimes.add(crime);
             mCrimeMap.put(crime.getId(), crime);
         }
     }
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        return new ArrayList<>(mCrimeMap.values());
     }
 
     public Crime getCrime(UUID id) {
